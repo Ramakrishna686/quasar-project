c3bb0bbaa1c6a9f0b87997b1478190541729b70b
Ramakrishna686 <ramkidasari@gmail.com>
2024-04-25 17:25:46 +0530

    j

--- src/components/tableData.vue
+++ src/components/tableData.vue
@@ -6,7 +6,7 @@
       title="Table Data"
       :rows="rows"
       :columns="columns"
-      row-key="name"
+      row-key="fileId"
       v-model:pagination="pagination"
     >
       <template v-slot:pagination="scope">
@@ -59,171 +59,7 @@
 <script>
 import { ref, computed } from "vue";
 
-const columns = [
-  {
-    name: "desc",
-    required: true,
-    label: "Dessert (100g serving)",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: "calories",
-    align: "center",
-    label: "Calories",
-    field: "calories",
-    sortable: true,
-  },
-  { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
-  { name: "carbs", label: "Carbs (g)", field: "carbs" },
-  { name: "protein", label: "Protein (g)", field: "protein" },
-  { name: "sodium", label: "Sodium (mg)", field: "sodium" },
-  {
-    name: "calcium",
-    label: "Calcium (%)",
-    field: "calcium",
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  },
-  {
-    name: "iron",
-    label: "Iron (%)",
-    field: "iron",
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  },
-];
-
-const rows = [
-  {
-    name: "Frozen Yogurt",
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: "14%",
-    iron: "1%",
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: "8%",
-    iron: "1%",
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: "6%",
-    iron: "7%",
-  },
-  {
-    name: "Cupcake",
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: "3%",
-    iron: "8%",
-  },
-  {
-    name: "Gingerbread",
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: "7%",
-    iron: "16%",
-  },
-  {
-    name: "Jelly bean",
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: "0%",
-    iron: "0%",
-  },
-  {
-    name: "Lollipop",
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: "0%",
-    iron: "2%",
-  },
-  {
-    name: "Honeycomb",
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: "0%",
-    iron: "45%",
-  },
-  {
-    name: "Donut",
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: "2%",
-    iron: "22%",
-  },
-  {
-    name: "KitKat",
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: "12%",
-    iron: "6%",
-  },
-];
-
-export default {
-  setup() {
-    const pagination = ref({
-      sortBy: "desc",
-      descending: false,
-      page: 2,
-      rowsPerPage: 3,
-      // rowsNumber: xx if getting data from a server
-    });
-
-    return {
-      pagination,
-
-      columns,
-      rows,
-
-      pagesNumber: computed(() => {
-        return Math.ceil(rows.length / pagination.value.rowsPerPage);
-      }),
-    };
-  },
-};
-</script>
-/* 
-{
+const apiResponse = {
     "content": [
         {
             "fileId": "3ec571bc-1760-4088-97fc-70da82e5796e",
@@ -287,4 +123,95 @@ export default {
     "first": true,
     "empty": false
 }
-*/
+
+const columns = [
+  {
+    name: "desc",
+    required: true,
+    label: "File Id",
+    align: "left",
+    classes: 'customEllipsis',
+    field: (row) => row.fileId,
+    sortable: true,
+  },
+  {
+    name: "userId",
+    classes: 'customEllipsis',
+    align: "center",
+    label: "User Id",
+    field: "userId",
+    sortable: true,
+  },
+  { name: "fileName", label: "File Name", field: "fileName",
+    classes: 'customEllipsis',
+   sortable: true },
+  { name: "status", label: "Status", field: "status" },
+  { name: "createdDate", label: "Created Date", field: "createdDate" }
+];
+
+const rows = [ {
+            "fileId": "3ec571bc-1760-4088-97fc-70da82e5796e",
+            "userId": "b56ef446-99a1-4ade-87ae-5bae165ee7bd",
+            "fileName": "Billing_Report_2024.23.04.csv",
+            "status": "PENDING",
+            "createdDate": "2024-04-23T16:50:58.785346+05:30"
+        },
+        {
+            "fileId": "4bbcf5fa-2ad3-4856-8dc9-36109ad7e2f7",
+            "userId": "b56ef446-99a1-4ade-87ae-5bae165ee7bd",
+            "fileName": "Billing_Report_2024.23.04.csv",
+            "status": "PENDING",
+            "createdDate": "2024-04-23T17:05:13.315902+05:30"
+        },
+        {
+            "fileId": "40f923ae-4a29-447a-9923-d17628891510",
+            "userId": "b56ef446-99a1-4ade-87ae-5bae165ee7bd",
+            "fileName": "Billing_Report_2024.23.04.csv",
+            "status": "PENDING",
+            "createdDate": "2024-04-23T17:21:13.545041+05:30"
+        },
+        {
+            "fileId": "32669166-5bc7-49da-bb64-48a82a9ae43a",
+            "userId": "b56ef446-99a1-4ade-87ae-5bae165ee7bd",
+            "fileName": "Billing_Report_2024.23.04.csv",
+            "status": "PENDING",
+            "createdDate": "2024-04-23T17:33:18.063927+05:30"
+        },
+        {
+            "fileId": "3d4916d7-36d9-43ae-b4f5-9ae3be785fcd",
+            "userId": "b56ef446-99a1-4ade-87ae-5bae165ee7bd",
+            "fileName": "Billing_Report_2024.23.04.csv",
+            "status": "PENDING",
+            "createdDate": "2024-04-23T18:11:10.779886+05:30"
+        }];
+
+export default {
+  setup() {
+    const pagination = ref({
+      sortBy: "desc",
+      descending: false,
+      page: 1,
+      rowsPerPage: 5,
+      // rowsNumber: xx if getting data from a server
+    });
+
+    return {
+      pagination,
+
+      columns,
+      rows,
+
+      pagesNumber: computed(() => {
+        return Math.ceil(rows.length / pagination.value.rowsPerPage);
+      }),
+    };
+  },
+};
+</script>
+<style>
+.customEllipsis {
+  text-overflow: ellipsis !important;
+  white-space: nowrap !important;
+  overflow: hidden !important;
+}
+</style>
